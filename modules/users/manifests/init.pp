1421cc3eadb9dba7cb9111f2353313b7fdec116c
Leo Shaw <github@leoshaw.co.uk>
2013-11-29 20:21:44 +0000

    Use bash shell

--- modules/users/manifests/init.pp
+++ modules/users/manifests/init.pp
@@ -9,6 +9,7 @@ class users {
     name   => 'leo@bsd43',
     user   => 'leo',
     type   => 'rsa',
+    shell  => '/bin/bash',
     key    => 'AAAAB3NzaC1yc2EAAAABIwAAAQEAxCt5JSGdcDvN0z2G6bUexl2uITdqlylC/2bj1o7JQUlGfM3uOSA1NBjVsMhdAJM4L921Ejt3btH4awesNWgY+XPgk2LJ/Qc4/+LRLZfDNaGYY/+Foin0/aOrffB8P4GULC+T2MpPaAX/b7vdM2uD3tEBH5uWRbIx1lRi4IEUhF2dfIpKqbfqnYrya+pIWhiTnX0Ah1teR3CoSd05jKMDTr9we8yFB48x4gbw6qUL4RWzSZEuQw3Et7G2GLJDiv8jORYVWAHQ5VsuUJA3LwK0nmdTqdyryPZX0WVxYST0uiFJRTEokSgtYlUZy9aymsoKbyoowsE1iyrzcuVNr8GBMw==',
   }
 }
