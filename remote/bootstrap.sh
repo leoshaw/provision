1f292980f04946a987cec5ba447baf937967abab
Leo Shaw <leo@leoshaw.co.uk>
2015-01-12 22:41:08 +0000

    Install git

--- remote/bootstrap.sh
+++ remote/bootstrap.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get -y install puppet build-essential ruby-dev
+sudo apt-get -y install puppet build-essential ruby-dev git
 sudo gem install librarian-puppet --no-ri --no-rdoc
 sudo librarian-puppet install --verbose
 sudo puppet apply --modulepath=./modules:./my_modules manifests/site.pp
