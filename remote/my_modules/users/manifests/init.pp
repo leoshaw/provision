3c9e47e3503d838218631a335e1bbb41cd893a25
Leo Shaw <github@leoshaw.co.uk>
2016-01-20 18:41:22 +0000

    Update public key

--- remote/my_modules/users/manifests/init.pp
+++ remote/my_modules/users/manifests/init.pp
@@ -9,9 +9,9 @@ class users {
 
   ssh_authorized_key { 'leo':
     ensure => present,
-    name   => 'leo@bsd43',
+    name   => 'leo@leoshaw.co.uk',
     user   => 'deployer',
     type   => 'rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC/t2j7wWkuXY2GF+Rl1+QVuiDfTkfNb7pD76cDxF3fV/yzbforiFoSSC7PTZHUR78O9pt2dN6D+iqG7xTmtdytm90ifCaDU8anTJ7gM56w1uiXiLkesbLcvO52EFAyRId0wxbTkq2M4yTIaLZvxW9JLql1ne2WpytreDNoNMlbrflnFsbAoTRJE/OC2Dfoy2nnPqhOvkr9jtO7AHKH34yW+eF0yOUP/E3IX9aBt1llfX2pP9X3a2pvM5C83ZlznOgFdwmgug0t39FLkkUdTsFKNIPCFwQaUQHsbYo+ESkGrBkclmNNiUd91+hk+74nnhm5oLOEZY9tkLJ94eFli+m3',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDagXE1pVGvlC1xA8+Zx6VCotZgodugifIk4S8+kZpOpThQNFNze9sz3OE9q1dhHdVXihImurGVW+XRWYv74qh7EnwTWmvJhmCq0zPudG5ZUEmvbKtEvSZyr6M8WGmQAgiNkPNAMZ4r3XcJ0shBzrY/Asqa/sMOzU5OkWOolUa8o1vYmmsXtnBvW/8U6XA4zc8GVClSitpe5NchceHzS7c16s3KrD/8ZhFxQVX9Ik/NNMdsXF7Whx8quBqBqUjdRUpkREAsHXMnCU+D1jsFjpVRMHRHuXDNubX2ZK72KnEymtVYmFdO7ocgt86FQT/JILR8CMTnGJb45YFGnQjNKkCGW/swyBu31+dZT7y1MoOUhdOl9uRfhPyVrolaSG+I+FHybUj+NdCByVZ4948hoquD/CeXG1lrOj1GsbhXh4mlJrWq1PMD7F/zTXxjDI+Cp/R9p4WBLkn4S8JfgfkYxTIdP9fsbDjWXYRwSfMlgRkBkEcYTqPZnDgA0VtWICT56jxYGgVnxCwStxKyLntszWEFgvsok6oVYtn+N/m6QT5u6jFG3yU/H846TtNlKhFolv3UwG5Y/x07YouUe35utt9VSPCTvvsUA2mbYSgNXpMTX4hEC1bH4dIBQWzkCoGiS40S6lEKqzXXq9Nsjj2sOKTb144HKalbSPeAYvHaW0scmQ==',
   }
 }
