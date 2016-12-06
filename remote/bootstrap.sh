95d55d1cd4b9a9ebcd1252b5073fd08b6eb07bdd
Leo Shaw <leo@leoshaw.co.uk>
2016-12-06 21:45:29 +0000

    Remove deprecated 'import' for newer versions of Puppet

--- remote/bootstrap.sh
+++ remote/bootstrap.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 sudo apt-get -y install puppet build-essential ruby-dev git
 sudo gem install librarian-puppet --no-ri --no-rdoc
 sudo librarian-puppet install --verbose
-sudo puppet apply --modulepath=./modules:./my_modules manifests/site.pp
+sudo puppet apply --modulepath=./modules:./my_modules manifests
